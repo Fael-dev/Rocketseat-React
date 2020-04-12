10ec758fc5d1d01dc3890022a6ff931c2093c68c
Rafael <faelleonan@gmail.com>
2020-04-12 20:47:51 -0300

    Finalizando curso introdutorio ao React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import './style.css';
+import Header from './components/Header';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
-}
+
+const App = () =>(
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello React</h1>
+//     </div>
+//   );
+// }
+
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+
+const Header = () => (
+    <header id="main-header">
+        JSHunt
+    </header>
+); 
+
+export default Header;
+
+// Outra forma de criar um componente, por uma classe
+// class Header extends Component{
+//     render(){
+//         return <h1> Componente </h1>
+//     }
+// }
